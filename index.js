0a9d90b5e13c7c6b24adefdec6cb80605b807811
apple <oladipupo@errandpay.com>
2024-04-02 16:52:24 +0100

    add hello and api hello endpoint

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const { createServer, } = require("http");
 
 const app = createServer((req, res) => {
-    res.end("server is working");
+
+    // hello html endpoint
+    if (req.url == "/hello"){
+        res.write("<b>Hello GDSC</b>");
+        res.end();
+    }
+    // hello api endpoint
+    else if (req.url == "/api/hello"){
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        })
+        .end("{\"message\":\"Hello GDSC\"}");
+    }
+    else{
+        res.end("server is working");
+    }
 });
+
 
 app.listen(3000, () => { console.log("server running on port 3000"); });
