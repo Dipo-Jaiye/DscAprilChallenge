bb735f8bcd28d7d08846975294546812856593ab
apple <oladipupo@errandpay.com>
2024-04-03 23:28:01 +0100

    add database connection, along with sample env file

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Day 1: Start a server in node.
 Run the server with `node index`  
   
 Day 2: Create hello html and api endpoint  
-Visit `localhost:3000/hello` for html endpoint and `localhost:3000/api/hello` for JSON response
+Visit `localhost:3000/hello` for html endpoint and `localhost:3000/api/hello` for JSON response  
+  
+Day 3: Connect to a database  
+A Redis database was provisioned and connected to. The app starts after the redis connection is made  
--- index.js
+++ index.js
@@ -1,23 +1,50 @@
+require('dotenv').config();
 const { createServer, } = require("http");
+const { createClient, } = require("redis");
+const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, } = process.env;
 
+// create node server
 const app = createServer((req, res) => {
 
     // hello html endpoint
-    if (req.url == "/hello"){
+    if (req.url == "/hello") {
         res.write("<b>Hello GDSC</b>");
         res.end();
     }
     // hello api endpoint
-    else if (req.url == "/api/hello"){
+    else if (req.url == "/api/hello") {
         res.writeHead(200, {
             'Content-Type': 'application/json'
         })
-        .end("{\"message\":\"Hello GDSC\"}");
+            .end("{\"message\":\"Hello GDSC\"}");
     }
-    else{
+    else {
         res.end("server is working");
     }
 });
 
+// create database client
+const client = createClient({
+    password: REDIS_PASSWORD,
+    socket: {
+        host: REDIS_HOST,
+        port: REDIS_PORT
+    }
+});
 
-app.listen(3000, () => { console.log("server running on port 3000"); });
+// add error handler for the database client
+client.on('error', error => {
+    console.error(`Redis client error:`, error);
+});
+
+// start server after database is connected
+client.on('ready', () => {
+    console.log("database connected...");
+    app.listen(3000, () => { console.log("server running on port 3000"); });
+});
+
+// use then/catch because an error throws when using await
+client.connect()
+.then(() => {
+
+});
